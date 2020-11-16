0c5c96d5ae36991f2a61b3327e5eebd8133fa206
Hiago Rios <hriosdev@gmail.com>
2020-11-16 11:05:06 -0300

    Ajustes na generalização de usuario

--- scripts/1 - Creates.sql
+++ scripts/1 - Creates.sql
@@ -1,9 +1,3 @@
--- drop table ministrante_evento;
--- drop table participante_evento;
--- drop table usuario;
--- drop table evento;
--- drop table ministrante;
-
 create role administrador with login superuser password '4dm1n157r4d0r';
 create role organizador with login password '0rg4n1z4d0r';
 create role participante with login password 'p4r71c1p4n73';
@@ -12,11 +6,15 @@ create database uesc_eventos with owner = administrador;
 ----------------------------------------------------------
 SET TIMEZONE TO 'America/Bahia';
 
+-- Ajustamos a generalização dos tipos de usuário, definindo como Categoria
+-- e replicando as permissões dos usuários de menor nível para os usuário de maior nível
+-- Também ajustamos o trigger before insert evento, para verificar se o usuário é organizador ou administrador
+
 create table usuario (
 	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	nome varchar(255) NOT NULL,
 	email varchar(255) unique NOT NULL,
-	senha varchar(255),
+	senha varchar(255) NOT NULL,
 	tipo_usuario char(1) NOT NULL,
 	cpf char(11) unique NOT NULL
 );
@@ -33,14 +31,14 @@ create table evento (
 	id_evento_pai integer REFERENCES evento(id),
 	id_organizador integer REFERENCES usuario(id) NOT NULL,
 	nome varchar(255) NOT NULL,
-	descricao text NOT NULL,
+	descricao text,
 	data_inicio timestamp with time zone NOT NULL,
 	data_fim timestamp with time zone NOT NULL,
 	local varchar(255) NOT NULL,
-	preco float(2) NOT NULL,
+	preco float(2),
 	qtd_vagas integer NOT NULL,
-	data_inicio_inscricao timestamp with time zone,
-	data_fim_inscricao timestamp with time zone
+	data_inicio_inscricao timestamp with time zone NOT NULL,
+	data_fim_inscricao timestamp with time zone NOT NULL
 );
 
 create table ministrante_evento (
@@ -55,12 +53,15 @@ create table participante_evento (
 	PRIMARY KEY(id_participante, id_evento)
 );
 
+-- PARTICIPANTE
 grant insert on table usuario to participante;
 grant insert, select, delete on table participante_evento to participante;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO participante;
+
+-- ORGANIZADOR
+grant insert, select on table usuario to organizador;
 grant insert, select, delete on table participante_evento to organizador;
 grant insert, select, update, delete on table ministrante to organizador;
 grant insert, select, delete on table ministrante_evento to organizador;
-grant select on table usuario to organizador;
 grant insert, select, update, delete on table evento to organizador;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO organizador;
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO participante;
--- scripts/4 - Trigger before insert evento.sql
+++ scripts/4 - Trigger before insert evento.sql
@@ -1,8 +1,8 @@
--- Antes de inserir um evento, é checado se o id_organizador realmente é de um organizador
+-- Antes de inserir um evento, é checado se o id_organizador realmente é de um organizador ou administrador
 CREATE OR REPLACE FUNCTION insert_evento_trigger_func() RETURNS TRIGGER AS
 $$ 
 BEGIN
-	IF NOT EXISTS (SELECT 1 from usuario where tipo_usuario = 'O' AND id = NEW.id_organizador) THEN
+	IF NOT EXISTS (SELECT 1 from usuario where (tipo_usuario = 'O' OR tipo_usuario = 'A') AND id = NEW.id_organizador) THEN
 		RAISE EXCEPTION 'Para cadastrar eventos é necessário ser um Organizador!';
 	END IF;
 	RETURN NEW;
